8822d827e1104f02c97567730551083ca09c7be8
levistinkt <sdestinkerd@gmail.com>
2024-04-19 11:35:06 +0200

    main pagina aangemaakt
    - added title
    - added content
    - added navigation (no files yet)
    - added layout inheritance

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title')</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+
+
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/gokken">Gokken</a></li>
+                <li><a href="/tussenstand">Tussenstand Checken</a></li>
+                <li><a href="/admin">Admin Login</a></li>
+                <li><a href="/user">User Login</a></li>
+                <li><a href="/register">User Registreren</a></li>
+            <ul>
+        </nav>
+    </header>
+
+    <main>
+        @yield('content')
+    </main>
+
+</body>
+</html>
