14ee78e16083debb5d348f1d93baf637b1a6c96f
levistinkt <sdestinkerd@gmail.com>
2024-05-30 15:18:28 +0200

    css gelinkt, volledig functioneel
    als alles rood is, dan werkt de css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,8 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title')</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+    <title>@yield('title')</title>
 </head>
 
 
@@ -21,6 +22,7 @@
             <ul>
         </nav>
     </header>
+
 
     <main>
         @yield('content')
